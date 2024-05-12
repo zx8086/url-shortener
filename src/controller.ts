9f438d1d20f7af84dbbe7e9c950a54e383d1931b
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-05-12 22:31:21 +0200

    Add URL Simple Validation

--- src/controller.ts
+++ src/controller.ts
@@ -1,14 +1,7 @@
-// src/controller.ts
 import Elysia from 'elysia';
 import { shortenUrl, fetchUrl } from './service.ts';
-
-// Define RequestBody and Error with shape your function expects
-interface RequestBody {
-  longUrl: string;
-}
-interface CustomError {
-  message?: string;
-}
+import { isURLValid } from './lib/utils';
+import type { RequestBody, CustomError } from './lib/interfaces';
 
 const app = new Elysia();
 
@@ -16,14 +9,17 @@ export const urlController = (app: Elysia) => {
   app.post("/shorten",
       async (context) => {
         try {
-          console.log("Attempting to shorten URL");
           const longUrl: string = (context.body as RequestBody).longUrl;
-          console.log("URL valid, attempting to shorten:", longUrl);
-          if (!longUrl) {
+          console.log("Received URL: ", longUrl);
+
+          // check if longUrl is provided and valid
+          if (!longUrl || !isURLValid(longUrl)) {
             context.set.status = 400;
             context.body = {message: "Invalid or no URL provided."};
-            return;
+            return context.body;
           }
+
+          console.log("URL valid, attempting to shorten:", longUrl);
           const result = await shortenUrl(longUrl);
           context.set.status = 200;
           return result;
@@ -31,8 +27,10 @@ export const urlController = (app: Elysia) => {
           console.error("Error during URL shortening:", (error as CustomError).message);
           context.set.status = 500;
           context.body = {message: (error as CustomError).message || 'Failed to shorten URL'};
+          return context.body;
         }
       });
+
   app.get("/:shortUrl", async (context) => {
     const shortId = context.params.shortUrl;
     const urlDoc = await fetchUrl(shortId);
@@ -44,6 +42,7 @@ export const urlController = (app: Elysia) => {
     } else {
       context.set.status = 404;
       context.body = { message: "Page not found" };
+      return context.body;
     }
   });
 };
