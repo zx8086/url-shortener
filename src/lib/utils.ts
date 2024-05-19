ed80c1ea613d4557abef4f70c5da408da06338cd
zx8086 <1085655+zx8086@users.noreply.github.com>
2024-05-19 11:28:36 +0200

    Improve isValidURL func tion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 // utils.ts
 
 export function isURLValid(url: string): boolean {
-  const urlPattern: RegExp = /^(http|https):\/\/[^ "]+$/;
-  return urlPattern.test(url);
+  const urlPattern: RegExp = /^(https?:\/\/)?(?:[a-zA-Z0-9]-?)*[a-zA-Z0-9]+(?:\.(?:[a-zA-Z0-9]-?)*[a-zA-Z0-9]+)*(?::\d{1,5})?(?:\/[^\s]*)?$/
+  try {
+    new URL(url);
+    return urlPattern.test(url);
+  } catch (_) {
+    return false;
+  }
 }
